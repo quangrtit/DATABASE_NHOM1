a8c88cce205e7f8332207770163240b65c5dc5ea
QUANG <quangkhongvan4444@gmail.com>
2024-11-02 16:50:53 +0700

    database nhóm 1

---
+++ SQL/SQLQuery_basic_demo.sql
@@ -0,0 +1,84 @@
+﻿/*
+Câu 1: Lấy thông tin về tất cả nhân viên  số điện thoại bắt đầu bằng '08'
+
+Select nhan_vien.IDnhanvien,NHAN_VIEN.tennv,NHAN_VIEN_SDT.SDT
+from NHAN_VIEN
+join NHAN_VIEN_SDT on NHAN_VIEN_SDT.IDnhanvien = NHAN_VIEN.IDnhanvien
+where NHAN_VIEN_SDT.SDT like '08%';
+*/
+/*
+Câu 2: Lấy thông tin chi nhánh ở Cầu Giấy
+select * from Chi_Nhanh where Diachi = 'Cau Giay';
+*/
+/*Câu 3: Lấy thông tin về tất cả khách hàng thường có hóa đơn có số tiền thanh toán lớn hơn 900 000 
+select *
+from KHACH_THUONG
+join KHACH_HANG on KHACH_THUONG.IDKH_thuong = KHACH_HANG.IDkhachhang
+join HOA_DON on KHACH_HANG.IDkhachhang = HOA_DON.IDkhachhang
+where HOA_DON.Tongtien > 900000
+*/
+/* Câu 4: Lấy thông tin về tất cả đơn vị cung cấp cung cấp sản phẩm có số lượng tồn kho lớn hơn 90 
+select * 
+from DonViCungCap
+join SAN_PHAM on DonViCungCap.IDdonvicungcap = SAN_PHAM.IDdonvicungcap
+where SAN_PHAM.Soluong > 90
+*/
+/* Câu 5: Lấy danh sách các hóa đơn được tạo bởi những khách hàng VIP (lâu năm), bao gồm thông tin về hóa đơn và khách hàng 
+SELECT KHACH_HANG.TenKH, HOA_DON.IDhoadon, Bao_gom.SoLuong
+FROM KHACH_HANG
+JOIN HOA_DON ON KHACH_HANG.IDkhachhang = HOA_DON.IDkhachhang
+JOIN Bao_gom ON HOA_DON.IDhoadon = Bao_gom.IDhoadon
+JOIN KHACH_LAU_NAM ON KHACH_HANG.IDkhachhang = KHACH_LAU_NAM.IDKH_vip;
+*/
+/*Câu 6:  Lấy danh sách các sản phẩm mà khách hàng "Vu Van Nhan" đã đặt hàng trong các hóa đơn: 
+SELECT KH.TenKH, SP.Tensanpham, BG.SoLuong
+FROM KHACH_HANG KH
+JOIN HOA_DON HD ON KH.IDkhachhang = HD.IDkhachhang
+JOIN Bao_gom BG ON HD.IDhoadon = BG.IDhoadon
+JOIN SAN_PHAM SP ON BG.IDsanpham = SP.IDsanpham
+WHERE KH.TenKH = 'Vu Van Nhan';
+*/
+/* Câu 7: Lấy danh sách tất cả sản phẩm được cung cấp bởi các đơn vị cung cấp ở chi nhánh 'Soc Son' 
+SELECT SAN_PHAM.Tensanpham, SAN_PHAM.Giaban, SAN_PHAM.Soluong, DonViCungCap.TenDVCC
+FROM SAN_PHAM
+JOIN DonViCungCap ON SAN_PHAM.IDdonvicungcap = DonViCungCap.IDdonvicungcap
+JOIN NHAN_VIEN ON SAN_PHAM.IDnhanvien = NHAN_VIEN.IDnhanvien
+JOIN Chi_Nhanh ON NHAN_VIEN.IDchinhanh = Chi_Nhanh.IDchinhanh
+WHERE Chi_Nhanh.Diachi = 'Soc Son';
+*/
+/* Câu 8:Lấy danh sách các đơn vị cung cấp và tổng số sản phẩm mà mỗi đơn vị cung cấp đã cung cấp 
+SELECT DonViCungCap.TenDVCC, COUNT(SAN_PHAM.IDsanpham) AS TongSoSanPham
+FROM DonViCungCap
+LEFT JOIN SAN_PHAM ON DonViCungCap.IDdonvicungcap = SAN_PHAM.IDdonvicungcap
+GROUP BY DonViCungCap.TenDVCC;
+*/
+/*Câu 9:  lấy thông tin khách hàng mua nhiều hơn 1 lần 
+select KHACH_HANG.IDkhachhang, KHACH_HANG.TenKH, COUNT(*) as So_Lan_mua
+from KHACH_HANG
+join HOA_DON on KHACH_HANG.IDkhachhang = HOA_DON.IDkhachhang
+group by KHACH_HANG.IDkhachhang, KHACH_HANG.TenKH
+having count(*) >1;
+*/
+/* Câu 10: Liệt kê những chi nhánh mà khách hàng có ID KH002 chưa mua hàng. 
+SELECT Chi_Nhanh.IDchinhanh FROM Chi_Nhanh
+EXCEPT
+(SELECT DISTINCT Chi_Nhanh.IDchinhanh
+FROM ((Chi_Nhanh INNER JOIN NHAN_VIEN
+ON Chi_Nhanh.IDchinhanh = NHAN_VIEN.IDchinhanh)
+INNER JOIN HOA_DON
+ON HOA_DON.IDnhanvien = NHAN_VIEN.IDnhanvien)
+INNER JOIN KHACH_HANG
+ON KHACH_HANG.IDkhachhang = HOA_DON.IDkhachhang
+WHERE KHACH_HANG.IDkhachhang = 'KH002'
+)
+*/
+/* Câu 11:  In ra tên và số tiền mỗi chi nhánh phải trả cho nhân viên 
+SELECT Chi_Nhanh.IDchinhanh,Sum(LUONG.Luongcung + LUONG.Luongthuong) as 'TỔNG TIỀN LƯƠNG CHI TRẢ CHO NHÂN VIÊN'
+FROM ((Chi_Nhanh INNER JOIN NHAN_VIEN
+ON NHAN_VIEN.IDchinhanh = Chi_Nhanh.IDchinhanh)
+INNER JOIN CONG_VIEC
+ON NHAN_VIEN.IDcongviec = CONG_VIEC.IDcongviec)
+INNER JOIN LUONG
+ON CONG_VIEC.IDcongviec = LUONG.IDcongviec
+GROUP BY Chi_Nhanh.IDchinhanh
+*/
---
+++ SQL/SQLQuery_create_table_and_insert_data.sql
@@ -0,0 +1,228 @@
+﻿/*
+CREATE TABLE Chi_Nhanh(
+	IDchinhanh varchar(10) Primary key not null,
+	Diachi VARCHAR(20),
+)
+
+CREATE TABLE Chu_Chi_Nhanh(
+	CCCD VARCHAR(10) PRIMARY KEY NOT NULL,
+	Ten VARCHAR(50),
+	DiaChi VARCHAR(30),
+	SDT VARCHAR(10) NOT NULL,
+	IDchinhanh varchar(10),
+	FOREIGN KEY (IDchinhanh) references Chi_Nhanh(IDchinhanh) ON DELETE CASCADE ON UPDATE CASCADE,
+)
+CREATE TABLE DonViCungCap(
+	IDdonvicungcap varchar(10) primary key not null,
+	TenDVCC varchar(30),
+	DiaChi varchar(30),
+	SDT varchar(10) not null, 
+)
+
+create TABLE PHIEU_DAT(
+	IDdathang VARCHAR(20) PRIMARY KEY NOT NULL,
+	CCCD varchar(10) not null,
+	IDdonvicungcap varchar(10) not null,
+	Thoigiandat date not null,
+	Soluongdat INT NOT NULL,
+	foreign key (CCCD) references Chu_Chi_Nhanh(CCCD) ON DELETE CASCADE ON UPDATE CASCADE,
+	foreign key (IDdonvicungcap) references DonViCungCap(IDdonvicungcap) ON DELETE CASCADE ON UPDATE CASCADE,
+)
+
+create table CONG_VIEC(
+	IDcongviec varchar(10) primary key not null,
+	Tencongviec varchar(20) not null,
+)
+
+create table LUONG(
+	IDcongviec varchar(10) unique not null references CONG_VIEC(IDcongviec) ON DELETE CASCADE ON UPDATE CASCADE,
+	Luongcung int not null,
+	Luongthuong int not null,
+	)
+
+create table NHAN_VIEN(
+	IDnhanvien varchar(10) not NULL primary key,
+	TenNV varchar(50) not null,
+	DiachiNV varchar(100) not null,
+	IDcongviec varchar(10) not null references CONG_VIEC(IDcongviec) ON DELETE CASCADE ON UPDATE CASCADE,
+	IDchinhanh varchar(10) not null references Chi_Nhanh(IDchinhanh) ON DELETE CASCADE ON UPDATE CASCADE,
+	)
+
+CREATE TABLE SAN_PHAM(
+  IDsanpham VARCHAR(10) NOT NULL PRIMARY KEY,
+  Tensanpham VARCHAR(30) NOT NULL,
+  Giaban INT NOT NULL,
+  Soluong INT NOT NULL,
+  IDdonvicungcap VARCHAR(10) NOT NULL REFERENCES DonViCungCap(IDdonvicungcap) ON DELETE CASCADE ON UPDATE CASCADE,
+  IDnhanvien varchar(10) NOT NULL REFERENCES NHAN_VIEN(IDnhanvien) ON DELETE CASCADE ON UPDATE CASCADE,
+  )
+CREATE TABLE KHACH_HANG(
+  IDkhachhang VARCHAR(20) NOT NULL PRIMARY KEY,
+  TenKH VARCHAR(50) NOT NULL,
+  Gioitinh VARCHAR(5) CHECK (Gioitinh IN ('Nam', 'Nu')),
+  )
+CREATE TABLE KHACH_HANG_SDT(
+   IDkhachhang VARCHAR(20) NOT NULL REFERENCES KHACH_HANG(IDkhachhang) ON DELETE CASCADE ON UPDATE CASCADE,
+   SDT varchar(10) not null,
+   )
+CREATE TABLE HOA_DON(
+  IDhoadon VARCHAR(20) NOT NULL PRIMARY KEY,
+  IDkhachhang VARCHAR(20) NOT NULL REFERENCES KHACH_HANG(IDkhachhang) ON DELETE CASCADE ON UPDATE CASCADE,
+  IDnhanvien VARCHAR(10) NOT NULL REFERENCES NHAN_VIEN(IDnhanvien) ON DELETE CASCADE ON UPDATE CASCADE,
+  NgayGD DATETIME NOT NULL,
+  Tongtien INT NOT NULL,
+  )
+
+CREATE TABLE Bao_gom (
+    IDhoadon VARCHAR(20) NOT NULL,
+    IDsanpham VARCHAR(10) NOT NULL,
+    SoLuong INT NOT NULL,
+    PRIMARY KEY (IDhoadon, IDsanpham),
+    FOREIGN KEY (IDhoadon) REFERENCES HOA_DON(IDhoadon),
+    FOREIGN KEY (IDsanpham) REFERENCES SAN_PHAM(IDsanpham),
+) 
+
+CREATE TABLE KHACH_THUONG(
+    IDKH_thuong VARCHAR(20) NOT NULL PRIMARY KEY,
+    Voucher VARCHAR(20) NOT NULL UNIQUE,
+    FOREIGN KEY(IDKH_thuong) REFERENCES KHACH_HANG(IDkhachhang) ON DELETE CASCADE ON UPDATE CASCADE,
+    )
+
+CREATE TABLE KHACH_LAU_NAM(
+  IDKH_vip VARCHAR(20) NOT NULL PRIMARY KEY,
+  Thethanhvien VARCHAR(20) NOT NULL UNIQUE,
+  FOREIGN KEY (IDKH_vip) REFERENCES KHACH_HANG(IDkhachhang) ON DELETE CASCADE ON UPDATE CASCADE,
+  ) 
+
+CREATE TABLE NHAN_VIEN_SDT(
+   IDnhanvien VARCHAR(10) NOT NULL REFERENCES NHAN_VIEN(IDnhanvien) ON DELETE CASCADE ON UPDATE CASCADE,
+   SDT VARCHAR(10) NOT NULL,
+   )
+
+BULK INSERT Chi_Nhanh
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Chi_Nhanh.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2            -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT Chu_Chi_Nhanh
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Chu_Chi_Nhanh.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT DonViCungCap
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Don_Vi_Cung_Cap.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT phieu_dat
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Phieu_Dat.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT cong_viec
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Cong_Viec.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT luong
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Luong.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT NHAN_VIEN
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Nhan_Vien.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT SAN_PHAM
+FROM 'D:\MY_PROJECT\BTL_DATABASE\San_Pham.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT KHACH_HANG
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Khach_Hang.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT KHACH_HANG_SDT
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Khach_Hang_SDT.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT HOA_DON
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Hoa_Don.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT BAO_GOM
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Bao_Gom.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT khach_thuong
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Khach_Thuong.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT khach_lau_nam
+FROM 'D:\MY_PROJECT\BTL_DATABASE\Khach_Lau_Nam.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+
+BULK INSERT nhan_vien_sdt
+FROM 'D:\MY_PROJECT\BTL_DATABASE\NHAN_VIEN_SDT.csv'
+WITH (
+    FIELDTERMINATOR = ',',  -- Dấu phân cách giữa các cột
+    ROWTERMINATOR = '\n',   -- Dấu phân cách giữa các hàng
+    FIRSTROW = 2           -- Bỏ qua hàng tiêu đề
+);
+*/
+
+
+
+
+
+
+
